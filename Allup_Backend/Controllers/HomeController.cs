146e889e52e9a938afe343ecd6ba7b7549cf1a17
GunelYusuf <91114321+GunelYusuf@users.noreply.github.com>
2022-02-03 03:17:41 +0400

    initilaize project

---
+++ Allup_Backend/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Allup_Backend.DAL;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Allup_Backend.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly Context _context;
+
+        public HomeController(Context context)
+        {
+            _context = context;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Allup_Backend/DAL/Context.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Allup_Backend.DAL
+{
+    public class Context:DbContext
+    {
+        public Context(DbContextOptions<Context> options):base(options)
+        {
+
+        }
+        
+    }
+}
