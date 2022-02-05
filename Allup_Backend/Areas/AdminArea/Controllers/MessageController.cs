79ddfe01c4c0e16b0f0cd09e99706b430a1a96a1
GunelYusuf <91114321+GunelYusuf@users.noreply.github.com>
2022-02-06 01:04:28 +0400

    feat: create Billing Address CRUD

---
+++ Allup_Backend/Areas/AdminArea/Controllers/MessageController.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Allup_Backend.DAL;
+using Allup_Backend.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Allup_Backend.Areas.AdminArea.Controllers
+{
+
+    [Area("AdminArea")]
+    public class MessageController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly Context _context;
+        private readonly IWebHostEnvironment _env;
+
+        public MessageController(Context context, IWebHostEnvironment env, UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+            _context = context;
+            _env = env;
+        }
+        public IActionResult Index()
+        {
+            List<BillingAddress> billingAddresses = _context.BillingAddresses.Include(m=>m.User).ToList();
+
+            return View(billingAddresses);
+        }
+
+
+        public async Task<IActionResult>  Detail(int? id)
+        {
+            List<BillingAddress> billingAddresses = _context.BillingAddresses.Include(m => m.User).ToList();
+            var messages = billingAddresses.Find(u => u.Id == id);
+
+            AppUser user = await _userManager.FindByIdAsync(messages.UserId);
+            messages.User = user;
+
+            return View(messages);
+
+        }
+        //Get
+        public async Task<IActionResult> Delete(int? id)
+        {
+
+            List<BillingAddress> billingAddresses = _context.BillingAddresses.Include(m => m.User).ToList();
+            var messages = billingAddresses.Find(u => u.Id == id);
+
+            AppUser user = await _userManager.FindByIdAsync(messages.UserId);
+            messages.User = user;
+            return View(messages);
+
+        }
+
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int id)
+        {
+            BillingAddress message = await _context.BillingAddresses.FindAsync(id);
+            _context.BillingAddresses.Remove(message);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+
+        }
+
+
+
+
+    }
+}
