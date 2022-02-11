72044116433b97bddf50f134d90a4afd9c7b5c10
GunelYusuf <91114321+GunelYusuf@users.noreply.github.com>
2022-02-11 23:55:45 +0400

    feat: Basket Controller

---
+++ Allup_Backend/ViewModels/BasketProduct.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Allup_Backend.ViewModels
+{
+    public class BasketProduct
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public double Price { get; set; }
+
+        public int Count { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public string UserId { get; set; }
+    }
+}
